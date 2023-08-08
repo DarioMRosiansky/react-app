0d668015b3972fca08d5be2b499155c97587da2b
Dario Rosiansky <dariutias@hotmail.com>
2023-08-08 20:26:50 -0300

    1_3

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './components/navbar'
+import Navbar from './components/navbar'
+
+
+
+function App() {
+  return (
+    <Navbar /> 
+    
+  )
+}
+
+/* <h1>Find the perfect destination</h1>
+
+   <p> Our app will help you find the perfect path for your next trip. With an easy-to-use 
+    interface and a host of itinerary options, planning your next trip has never been easier.</p>
+
+</Main> */ 
+
+export default App
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,22 @@
+
+import './style.css'; 
+
+export default function Navbar() {
+
+return (
+    <nav>
+    <div class="navbar-title">
+      My Tinerary
+    </div>
+    <div class="navbar-links">
+      <a href="#">Home</a>
+      <a href="#">Cities</a>
+      <button class="login-button"> <img src="src/assets/user.png" height="15" width="15"/>  Login</button>
+    </div>
+  </nav>
+)
+}
+
+
+
+
